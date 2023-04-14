52701fc49287a418a4daa89c1cd40e3d16686296
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-04-14 04:56:39 -0700

    add main file

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+
+int main(int ac, char **av, char **env)
+{
+    int chars;
+    size_t buff_size = 0;
+    char *prompt = "=>> ";
+    char *buffer = NULL;
+    char *tok;
+    char **tokens;
+
+	while (1)
+	{
+		printf("%s", prompt);
+		chars = getline(&buffer, &buff_size, stdin);
+
+        /* handle EOF */
+		if (chars < 0)
+		{
+			return (0);
+		}
+
+        tok = strtok(buffer , " \n");
+
+        tokens = extract_args(tok);
+
+		execute(tokens, av[0]);
+	}
+    return 0;
+}
+
