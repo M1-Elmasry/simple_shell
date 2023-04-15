a6455dbd3e2838b89df5c5e05f3f0d84bc7c05e1
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-04-15 05:00:43 -0700

    add builtin commands

--- main.c
+++ main.c
@@ -1,17 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "main.h"
 
 
 int main(int ac, char **av, char **env)
 {
-	int chars;
+	int chars, i;
 	size_t buff_size = 0;
 	char *prompt = "=>> ";
 	char *buffer = NULL;
 	char *tok;
 	char **tokens;
+	builtin_funcs cmds[] = {
+		{"help", &sh_help}, {"cd", &sh_cd},
+		{"exit", &sh_exit},{"env", &sh_env}
+	};
 
 	while (1)
 	{
@@ -28,6 +29,15 @@ int main(int ac, char **av, char **env)
 
 		tokens = extract_args(tok);
 
+		/* executing the builtin commands */
+/*		for (i = 0; i < sizeof(cmds) / sizeof(cmds[0]); i++)
+		{
+			if (tokens[0] == cmds[i][0])
+			{
+				cmds[i][1](tokens);
+			}
+		}
+*/
 		execute(tokens, av[0]);
 	}
 	return 0;
