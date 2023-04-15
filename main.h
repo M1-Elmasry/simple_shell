2f10e0ec5b13d23b6674315b7dfe61989e9d6084
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-04-15 05:05:03 -0700

    add builtin functions and projct preproccessors

--- main.h
+++ main.h
@@ -1,7 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
 #ifndef MAIN_H
 #define MAIN_H
 
 char **extract_args(char *tok);
-int execute(char *args[], char *filename);
+void execute(char *args[], char *filename);
+int sh_cd(char **args);
+int sh_help(char **args);
+int sh_exit(char **args);
+int sh_env(char **args);
+
+typedef struct builtins
+{
+	char *command_name;
+	int (*fun)(char **);
+
+} builtin_funcs;
 
 #endif
