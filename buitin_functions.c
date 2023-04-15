51dd32afc47334992b0884a45a29533b1c98261d
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-04-15 05:11:53 -0700

    add builtin commands impelementation file

---
+++ buitin_functions.c
@@ -0,0 +1,20 @@
+int sh_cd(char **args)
+{
+	return (0);
+}
+
+int sh_help(char **args)
+{
+    return (0);
+}
+
+int sh_exit(char **args)
+{
+    return (0);
+}
+
+int sh_env(char **args)
+{
+    return (0);
+}
+
