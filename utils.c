9c530834896ec572b9fa0b6b2a29c64517a96f60
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-04-24 20:09:40 +0200

    extract_args betty discription

--- utils.c
+++ utils.c
@@ -1,9 +1,15 @@
 #include "main.h"
 
+/**
+ * extract_args - argument extraction by tokenization
+ * @tok: tokens
+ * @delim: deliminations
+ * Return: pointer to chars
+ */
 char **extract_args(char *tok, char *delim)
 {
 	int buffer_size = 8, i = 0;
-	char **args = malloc((buffer_size + 1) * sizeof(char*));
+	char **args = malloc((buffer_size + 1) * sizeof(char *));
 
 	if (!args)
 	{
@@ -16,7 +22,7 @@ char **extract_args(char *tok, char *delim)
 		if (i >= buffer_size)
 		{
 			buffer_size *= 2;
-			args = realloc(args, (buffer_size + 1) * sizeof(char*));
+			args = realloc(args, (buffer_size + 1) * sizeof(char *));
 			if (!args)
 			{
 				perror("allocation failed 1");
