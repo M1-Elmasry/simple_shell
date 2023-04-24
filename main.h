6bcb2a474e52c4c2400519d4b0e35ab368a35f28
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-04-24 20:53:40 +0200

    prototype of getline function

--- main.h
+++ main.h
@@ -15,7 +15,7 @@
 
 typedef int (*f_ptr)(char **);
 
-
+ssize_t my_getline(char **lineptr, size_t *n, FILE *stream);
 char **extract_args(char *tok, char *delim);
 int execute(char *args[], char **argv, char **env);
 char *search_cmd(char *cmd);
