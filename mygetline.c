0f2fc3c7cf3be91e817009cdad6459cc86ad88fe
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-04-24 21:10:39 +0200

    code update

--- mygetline.c
+++ mygetline.c
@@ -2,40 +2,40 @@
 
 ssize_t my_getline(char **lineptr, size_t *n, FILE *stream)
 {
-	size_t buffer_size = 0;
-	ssize_t bytes_read = 0;
+	size_t size = 0;
+	ssize_t read = 0;
 	char *buffer = NULL;
 	int c;
 
 	if (lineptr == NULL || n == NULL || stream == NULL)
 		return (-1);
 	buffer = *lineptr;
-	buffer_size = *n;
+	size = *n;
 
 	while ((c = getc(stream)) != EOF)
 	{
-		if (bytes_read + 1 > buffer_size)
+		if (read + 1 > size)
 		{
-			size_t new_size = buffer_size * 2;
+			size_t new_size = size * 2;
 
-			if (new_size < bytes_read + 1)
-				new_size = bytes_read + 1;
+			if (new_size < read + 1)
+				new_size = read + 1;
 			char *new_buffer = realloc(buffer, new_size);
 
 			if (new_buffer == NULL)
 				return (-1);
 			buffer = new_buffer;
-			buffer_size = new_size;
+			size = new_size;
 			*lineptr = buffer;
-			*n = buffer_size;
+			*n = size;
 		}
-		buffer[bytes_read++] = c;
+		buffer[read++] = c;
 		if (c == '\n')
 			break;
 	}
-	if (bytes_read == 0)
+	if (read == 0)
 		return (-1);
-	buffer[bytes_read] = '\0';
-	return (bytes_read);
+	buffer[read] = '\0';
+	return (read);
 }
 
