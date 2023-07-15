6811643f626d5ec59068e75440c916e0f7222aec
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-07-15 18:50:36 +0300

    main.h

--- main.h
+++ main.h
@@ -8,10 +8,12 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <sys/stat.h>
 
 int execute(char **tokens, char *argv[], char **env);
 int tokens_num(char *buffer, char *delim);
 char **parse(char *buffer, char *delim);
+char *search(char *command);
 
 
 #endif
