faae2b81feb27093f88aa7d90d286b8a72ef8567
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-07-25 07:36:34 +0300

    update main.c

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
  */
 int main(__attribute__((unused)) int argc, char *argv[], char *envp[])
 {
-	int readed_chars;
+	int readed_chars, status = 0;
 	char *buffer = NULL;
 	char **tokens = NULL;
 	char prompt[2] = "$ ";
@@ -23,20 +23,26 @@ int main(__attribute__((unused)) int argc, char *argv[], char *envp[])
 
 		/* get input from the user */
 		readed_chars = getline(&buffer, &buff_size, stdin);
+		/* exit from shell if user clicked ctrl + d */
 		if (readed_chars == -1 || readed_chars == EOF)
-		{
 			break;
-		}
 
 		tokens = parse(buffer, " \n");
 		if (tokens[0] != NULL)
 		{
 			tokens[0] = search(tokens[0]);
-			execute(tokens, argv, envp);
-		}
-	}
+			status = execute(tokens, argv, envp);
 
+			if (status == -1) /* exit command entered */
+				break;
+		}
+		free_dptr(tokens);
+		/* break the loop and exit when error eccur in noninteractive mode */
+		if (!isatty(STDIN_FILENO) && status)
+			break;
+	}
 	free(buffer);
-	free(tokens);
-	return (0);
+
+	status = handle_program_return_value(tokens, argv, status);
+	return (status);
 }
