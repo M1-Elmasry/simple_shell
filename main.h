942a0b80d648c382c14f26fb306a25862809f30f
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-07-25 07:38:52 +0300

    update prototypes

--- main.h
+++ main.h
@@ -10,11 +10,46 @@
 #include <string.h>
 #include <sys/stat.h>
 
-int execute(char **tokens, char *argv[], char **env);
-int tokens_num(char *buffer, char *delim);
+
+/* parse.c */
+int count_tokens(char *buffer, char *delim);
+void handle_comments(char **tokens);
 char **parse(char *buffer, char *delim);
+
+/* search.c */
 char *search(char *command);
+
+/* exec.c */
+int execute(char **tokens, char *argv[], char **env);
+
+/* builtins.c */
+int (*is_builtin(char *cmd))(char **, char **, char **);
+int exec_builtin(int (*func)(char **, char **, char **), char **tokens, char **env, char **argv);
+int handle_exit_args(char **tokens, char **argv);
+int builtin_exit(__attribute__((unused)) char **tokens, __attribute__((unused)) char **env, __attribute__((unused)) char **argv);
+int builtin_env(__attribute__((unused)) char **tokens, char **env, __attribute((unused)) char **argv);
+int builtin_cd(char **tokens, __attribute__((unused)) char **env, char **argv);
+
+/**
+ * struct builtin_t - structure to hold
+ * the builtin command and their functions
+ * @cmd: pointer to string (command name)
+ * @func: command function
+ */
+typedef struct builtin_t
+{
+	char *cmd;
+	int (*func)(char **, char**, char **);
+} builtin_s;
+
+/* utils.c */
+
+void print_error(char *program_name);
+void free_dptr(char **dptr);
+int _atoi(char *str);
+int handle_program_return_value(char **tokens, char **argv, int status);
 
 
 #endif
+
 
